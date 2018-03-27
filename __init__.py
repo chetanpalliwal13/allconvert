6dbdfb4bebb31d6d869a1659d7c8fee3d38d987c
Chetan Palliwal <33956272+chetanpalliwal13@users.noreply.github.com>
2018-03-27 23:51:03 +0530

    Add files via upload
    other supportable files

---
+++ __init__.py
@@ -0,0 +1,2 @@
+# API To support all list operation to tuple which not available normally in python.
+# e.g -> insert, append, copy, clear, pop, remove, reverse, sort
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tupleo',
+      version='1.0.0',
+      description='A tuple operations API',
+      url='https://github.com/chetanpalliwal13/tupleo',
+      author='chetanpalliwal13',
+      author_email='chetanpalliwal13@gmail.com',
+      license='GPL',
+      packages=['tupleo'])
