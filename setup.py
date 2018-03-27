e3b5cd45e8902229bf1a1422faa5667a9c875b05
Chetan Palliwal <33956272+chetanpalliwal13@users.noreply.github.com>
2018-03-27 23:52:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='tupleo',
       version='1.0.0',
-      description='A tuple operations API',
-      url='https://github.com/chetanpalliwal13/tupleo',
+      description='API to provide functionality of conversion between data types',
+      url='https://github.com/chetanpalliwal13/allconvert',
       author='chetanpalliwal13',
       author_email='chetanpalliwal13@gmail.com',
       license='GPL',
-      packages=['tupleo'])
+      packages=['allconvert'])
